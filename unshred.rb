2b9883df31987506ccc9d9bcafbffb2b401e4342
Joshua Davison <josh@stringbot.com>
2011-11-19 20:46:46 -0600

    almost almost

--- unshred.rb
+++ unshred.rb
@@ -15,8 +15,12 @@ class UnshredApp < Processing::App
     strips = load_strips(image)
     matches = find_matches(strips)
 
-    reassembled_image = reassemble(nil, matches)
-    image(reassembled_image, 0, 0)
+    matches.each do |match|
+      puts match.inspect
+    end
+
+    reassembled_image = reassemble(matches)
+    image(reassembled_image.image, 0, 0)
   end
 
   def load_strips(image)
@@ -36,10 +40,9 @@ class UnshredApp < Processing::App
       if collision = matches[match.left]
         matches[match.left] = resolve_collision(collision, match)
       end
-      puts match.inspect
       matches[match.left] = match
     end
-    matches
+    matches.values
   end
 
   # if the new distance is less than the old distance
@@ -54,12 +57,39 @@ class UnshredApp < Processing::App
     end
   end
 
-  def reassemble(strip, matches)
+  def reassemble(matches)
+    # we're finding the left edge by identifying the strip which appears
+    # as a left side of a match but doesn't appear as a right side
+    #
+    # mathy shortcut: n + (n-1) + (n-2) ... + (n-n) => (n + 1) * (n/2)
+    length = matches.length
+    allsum = (length + 1) * (length * 0.5)
+
+    # the sum of all the indices in the array minus the sum of
+    # all the right indices reveals the one missing index
+    realsum = matches.inject(0) {|sum,m| sum + m.right.index}
+    missing_index = (allsum - realsum).floor
+
+
+    left_match = matches.find {|m| m.left.index == missing_index }
+    matches.delete left_match
+
+    reassemble2(left_match.left, left_match, matches)
+  end
+
+  def reassemble2(strip, left_match, matches)
     return strip if matches.empty?
 
-    key, value = matches.first
+    right = left_match.right
+    next_left_index = right.index
+    strip.join_right(right)
 
+    next_left = matches.find { |m| m.left.index == next_left_index }
+    matches.delete next_left
+    puts "Next left index: %s" % next_left.left.index
+    reassemble2(strip, next_left, matches)
   end
+
 end
 
 UnshredApp.new :title => "Unshreddin"
